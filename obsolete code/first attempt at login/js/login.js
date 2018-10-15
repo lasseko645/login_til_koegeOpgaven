c2c085e1e2d2174c2c2dc2aaed153d83f3f95970
lasseko645 <31951011+lasseko645@users.noreply.github.com>
2018-10-15 11:38:05 +0200

    gave up on obsolete code attempting again to realize a login function in a new way

--- obsolete code/first attempt at login/js/login.js
+++ obsolete code/first attempt at login/js/login.js
@@ -2,12 +2,12 @@
 //create the function for the onclick of our id="loginButton"
 function clickedLoginButton(){
 //local variables to hold our values for the login process
-let user = document.getElementById('username')
-let pass = document.getElementById('password')
+let user = document.getElementById('Username')
+let pass = document.getElementById('Password')
 
 //just a couple of test values for the idea to come trough since we have no database or other means to verify an account
-let testUser = "admin"
-let testPass = "12345"
+let testUser = "test"
+let testPass = "123"
 
 //this is the if statement that wil explains the logic of the system login function
 if(user == testUser){
@@ -18,7 +18,7 @@ if(user == testUser){
 
     }   else{
 
-        window.alert('forkert kodeord eller brugernavn')
+        window.alert('forkert kodeord')
 
     }
 
